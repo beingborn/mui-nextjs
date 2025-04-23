40de2e15f9954e7ac6a69d57820c4105bcf75c3f
beingborn <dolmin0708@naver.com>
2025-04-24 00:03:47 +0900

    Lnb Content 임시 데이터 저장, toggle 임시 진행

--- src/app/layouts/Sidebar.js
+++ src/app/layouts/Sidebar.js
@@ -3,6 +3,10 @@ import {root} from '../global-variables'
 import {Logo} from '../assets/logo/index'
 import { Typography } from '@mui/material'
 import Link from 'next/link'
+import {useState, useEffect} from 'react'
+
+// Lnb 임시 데이터
+import LnbContent from '../assets/data/LnbContent'
 
 const LnbAside = styled.aside`
     flex-basis: 260px;
@@ -26,61 +30,42 @@ const LogoWrap = styled.div`
 `
 
 const LnbBody = styled.div``
+const LnbDepth1List = styled.ul``
+const LnbDepth1 = styled.li`
+    &:not(:first-child) > button {
+        border-top: 1px solid #000;
+    }
 
+    &:last-child > button {
+        border-bottom: 1px solid #000;
+    }
 
+    > button {
+        width: 100%;
+        line-height: 40px;
+        padding: 0 16px;
+        text-align: left;
+        
+    }
+`
+const LnbDepth2List = styled.ul`
+    display: none;
+    border: 1px solid red;
 
-
-const lnbContent = [
-    { 
-        name: '콘텐츠',   
-        href : '/pages/about',
-        list : [
-            {
-                name: '콘텐츠관리',
-                href: '/pages/content/'
-            }
-            ,
-            {
-                name: '설문관리',
-                href: '/pages/content/'
-            },
-            {
-                name: '과제관리',
-                href: '/pages/content/'
-                
-            }
-        ]
-    },
-    { 
-        name: '강사 관리',
-        href : '/pages/ui',
-        list : [
-            {
-                name: '강사관리',
-                href: '/pages/about/'
-            }
-            ,
-            {
-                name: '강사 Q&A',
-                href: '/pages/about/'
-            },
-            {
-                name: '강사 페이지 관리',
-                href: '/pages/about/'
-                
-            }
-        ]
-    },
-    { 
-        name: 'Icons',
-        href : '/pages/icons'
-    },
-    { name: 'Cetner', href : '/pages/icons'},
-    { name: 'Cetner22', href : '/pages/icons'},
-]
+    &.is-active {
+        display: block;
+    }
+`
+const LnbDepth2 = styled.li``
 
 
 export default function Sidebar() {
+    const [isActive, setisActive] = useState(null);
+    const toggleActive = (index) => {
+        // prev 이전 값
+        setisActive(prev => (prev === index) ? null : index)
+    }
+
     return (
         <LnbAside>
             <LnbHeader>
@@ -91,34 +76,38 @@ export default function Sidebar() {
             </LnbHeader>
             <LnbBody>
                 <nav>
-                    <ul>
+                    <LnbDepth1List>
                         {
-                            lnbContent.map((lnb1depth, index)=> {
+                            LnbContent.map((lnb1depth, index)=> {
                                 return lnb1depth.list ? (
-                                    <li key={index}>
-                                        {lnb1depth.name}
-                                        <ul>
+                                    <LnbDepth1 key={index}>
+                                        <button onClick={() => toggleActive(index)}>
+                                                {lnb1depth.name}
+                                        </button>
+                                            <LnbDepth2List className={index === isActive ? 'is-active' : ''}>
                                             {
                                                 lnb1depth.list.map((lnb2depth, index)=> {
                                                     return (
-                                                        <li key={index}>
-                                                            <Link>
+                                                        <LnbDepth2 key={index}>
+                                                            <Link href={lnb2depth.href}>
                                                                 {lnb2depth.name}
                                                             </Link>
-                                                        </li>
+                                                        </LnbDepth2>
                                                     )
                                                 })   
                                             }
-                                        </ul>
-                                    </li>
+                                        </LnbDepth2List>
+                                    </LnbDepth1>
                                 ) : (
-                                    <li key={index}>
-                                        {lnb1depth.name}
-                                    </li>
+                                    <LnbDepth1 key={index}>
+                                        <button>
+                                            {lnb1depth.name}
+                                        </button>
+                                    </LnbDepth1>
                                 )
                             })
                         }
-                    </ul>
+                    </LnbDepth1List>
                 </nav>
             </LnbBody>
         </LnbAside>
