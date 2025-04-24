2eeb87736db5b87f4a6fdf267fdd2b90b1e13764
beingborn <dolmin0708@naver.com>
2025-04-24 18:22:30 +0900

    실습 목록 변경

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import CardContent from '@mui/material/CardContent'
 const TempUl = styled.ul`
   display: flex;
   flex-direction: column;
-  gap: 8px;
+  gap: 12px;
   font-size: 18px;
 `
 
@@ -22,13 +22,13 @@ export default function Home() {
                 <h2 style={{fontSize: '28px', marginBottom: '20px'}}>실습 목록</h2>
                 <TempUl>
                   <li>컴포넌트 설계</li>
-                  <li>라이브 서버</li>
                   <li>프론트 폴더 구조 생성</li>
                   <li>Layout 페이지 공통 출력</li>
                   <li>라우팅 페이지 이동</li>
                   <li>라우팅별 브레드크럼브, 타이틀 동적 출력</li>
                   <li>임시 데이터 사용 아코디언 기능 구현</li>
                   <li>SVG 공통 컴포넌트 라이브러리 사용</li>
+                  <li>공통 Util 함수 사용</li>
                   <li>MUI 컴포넌트 커스텀 사용</li>
                 </TempUl>
               </CardContent>
