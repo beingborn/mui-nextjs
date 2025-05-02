5df370b6288a122c648885e2b8eb1de4ea17ab0e
beingborn <dolmin0708@naver.com>
2025-05-02 09:16:14 +0900

    사이드바 액티브 오류 값 변경

--- src/app/layout/Sidebar/SidebarSection.js
+++ src/app/layout/Sidebar/SidebarSection.js
@@ -1,42 +1,38 @@
 import { useSearchParams } from 'next/navigation';
 
-import {
-    SidebarNav,
-    SidebarDepth1List,
-} from './styles'
+import { SidebarNav, SidebarDepth1List } from './styles';
 
 import SidebarItem from './SidebarItem';
 import useActive from '../../utils/useActive';
 
+export default function SidebarSection({ sidebarData }) {
+    const activeParams = useSearchParams();
+    const active = activeParams.get('active');
+    const active2 = activeParams.get('active2');
 
-export default function SidebarSection({sidebarData}){
-    const activeParams = useSearchParams()
-    const active = activeParams.get('active')
-    const active2 = activeParams.get('active2')
-
-    const depth1Active = useActive(active)    
-    const depth2Active = useActive(active2)
+    const depth1Active = useActive(Number(active));
+    const depth2Active = useActive(Number(active2));
 
     const depth2Init = () => {
-        depth2Active.toggleActive(null)
-    }
-    
+        depth2Active.toggleActive(null);
+    };
+
     return (
         <SidebarNav>
             <SidebarDepth1List>
-                {sidebarData.map((item,index) => {
+                {sidebarData.map((item, index) => {
                     return (
-                        <SidebarItem 
-                        data={item} 
-                        idx={index} 
-                        key={index}
-                        depth1Active={depth1Active}
-                        depth2Active={depth2Active}
-                        depth2Init={depth2Init}
+                        <SidebarItem
+                            data={item}
+                            idx={index}
+                            key={index}
+                            depth1Active={depth1Active}
+                            depth2Active={depth2Active}
+                            depth2Init={depth2Init}
                         />
-                    ) 
+                    );
                 })}
             </SidebarDepth1List>
         </SidebarNav>
-    )
+    );
 }
